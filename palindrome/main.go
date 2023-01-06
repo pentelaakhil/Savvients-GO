7d69bd8ad444c5d3a0149eacbc7e2cd65c2140c5
pentelaakhil <121001656+pentelaakhil@users.noreply.github.com>
2023-01-06 10:26:41 -0700

    leet code2 01/06/2023

---
+++ palindrome/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	var sum int = 0
+	var compare = x
+	for x > 0 {
+		r := x % 10
+		sum = (sum * 10) + r
+		x = x / 10
+
+	}
+	if compare == sum {
+		return true
+	}
+	return false
+}
+func main() {
+	fmt.Println(isPalindrome(121))
+	fmt.Println(isPalindrome(-121))
+	fmt.Println(isPalindrome(10))
+
+}
